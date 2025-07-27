98fec9ac9d072f94f24f0ac1bff2520118a721ae
Farhan Ahmad <farhanahmadptn@gmail.com>
2025-07-27 22:54:40 +0530

    Your commit message

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
    items-center flex-col'>
      <nav className=' flex justify-between items-center mb-10 w-full p-3'>
        <img src={logo} className=' w-28 object-contain' alt='logo'/>
-       <button onClick={()=>window.open('https://github.com/Farhan9373')} className=' black_btn'>Github</button>
+       {/* <button onClick={()=>window.open('https://github.com/Farhan9373')} className=' black_btn'>Github</button> */}
      </nav>
      <h1 className='head_text'>Summarize Articles With
         <br className=''/><span className='orange_gradient'>OpenAI GPT-4</span>
